4cc5398a4b0bbeddf8356fd5b1f91308ee741167
Victor Hernandez <victor.993_@hotmail.com>
2023-06-01 08:27:08 -0500

    GAP-39 added configurations for deployment

--- src/config.py
+++ src/config.py
@@ -6,9 +6,9 @@ if os.getenv('DEBUG', "true").lower() == "true":
     config['DEBUG'] = True
     config['HOST'] = 'localhost'
     config['PORT'] = 5000
-    config['CONNECTION_DB']='mongodb://root:s3cr3t@localhost:27017/dbgap?authSource=admin'
+    config['CONNECTION_DB']='mongodb://localhost:27017/dbgap'
 else:
     config['DEBUG'] = False
     config['HOST'] = '0.0.0.0'
-    config['PORT'] = os.getenv('PORT')
+    config['PORT'] = os.getenv('API_SPCAT_PORT')
     config['CONNECTION_DB']=os.getenv('CONNECTION_DB')
